b2c166264558d04b24885ad262526ad5bee7ff39
digitalh2o <digitalh2o@gmail.com>
2018-05-29 17:33:15 -0500

    Added Redux / Redux Thunk

---
+++ actions/auth_actions.js
@@ -0,0 +1,16 @@
+import { AsyncStorage } from "react-native";
+import { FACEBOOK_LOGIN_SUCCESS } from "./types";
+
+// How to use AsyncStore
+// AsyncStorage.setItem('fb_token', token);
+// AsyncStorage.getItem('fb_token')
+
+export const facebookLogin = () => async dispatch => {
+  let token = await AsyncStorage.getItem("fb_token");
+
+  if (token) {
+    // Dispatch an action saying FB login is done
+  } else {
+    // Start up FB login process
+  }
+};
