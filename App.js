bca79d4eb478dbbe9e0837d9e4727825a295d25a
digitalh2o <digitalh2o@gmail.com>
2018-05-28 19:03:24 -0500

    Basic navigation added between review and settings

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { createBottomTabNavigator } from "react-navigation";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
 
 import AuthScreen from "./screens/AuthScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ReviewScreen from "./screens/ReviewScreen";
 
 export default class App extends React.Component {
   render() {
     const MainNavigator = createBottomTabNavigator({
       welcome: { screen: WelcomeScreen },
-      auth: { screen: AuthScreen }
+      auth: { screen: AuthScreen },
+      main: {
+        screen: createBottomTabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: createStackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        })
+      }
     });
 
     return (
