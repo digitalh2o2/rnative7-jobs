49c121f4c75f6d95fa0e37c18232b5dc447158c4
digitalh2o <digitalh2o@gmail.com>
2018-05-30 13:30:26 -0500

    calling api successfully. display to user next with old deck component

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-import reverseGeocode from "latlng-to-zip";
+//import reverseGeocode from "latlng-to-zip";
 import qs from "qs";
 
 import { FETCH_JOBS } from "./types";
 
 const JOB_ROOT_URL = "http://api.indeed.com/ads/apisearch?";
 const JOB_QUERY_PARAMS = {
-  publisher: "4201738803816157",
+  publisher: "9723155516428820",
   format: "json",
   v: "2",
   latlong: 1,
@@ -14,12 +14,48 @@ const JOB_QUERY_PARAMS = {
   q: "javascript"
 };
 
+const GOOGLE_API_KEY = "AIzaSyCLwwJL71c06R1OZE11ZtRe17sceWvmYEw";
+const GEOCODE_ROOT_URL = `https://maps.googleapis.com/maps/api/geocode/json?`;
+
+const buildGeocodeUrl = ({ longitude, latitude }) => {
+  const query = qs.stringify({
+    key: GOOGLE_API_KEY,
+    latlng: `${latitude},${longitude}`
+  });
+  return `${GEOCODE_ROOT_URL}${query}`;
+};
+
+const reverseGeocode = async region => {
+  const url = buildGeocodeUrl(region);
+  let { data } = await axios.get(url);
+
+  if (data.error_message) {
+    throw new Error(data.error_message);
+  }
+
+  const { results } = data;
+
+  if (results.length === 0) {
+    throw new Error("No Results");
+  }
+
+  const postCode = results[0].address_components.find(
+    component => component.types[0] === "postal_code"
+  );
+
+  if (!postCode) {
+    throw new Error("No Postcode");
+  }
+
+  return postCode.long_name || postCode.short_name;
+};
+
 const buildJobsUrl = zip => {
   const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
   return `${JOB_ROOT_URL}${query}`;
 };
 
-export const fetchJobs = region => async dispatch => {
+export const fetchJobs = (region, callback) => async dispatch => {
   try {
     let zip = await reverseGeocode(region);
     const url = buildJobsUrl(zip);
@@ -28,7 +64,7 @@ export const fetchJobs = region => async dispatch => {
       type: FETCH_JOBS,
       payload: data
     });
-    console.log(data);
+    callback();
   } catch (e) {
     console.error(e);
   }
